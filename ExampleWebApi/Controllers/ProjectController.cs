0b0a414f3cb61b24ef08c52f4053f45201db00ef
levig <levi.goossens107@gmail.com>
2025-12-24 13:41:20 +0100

    add project controller

---
+++ ExampleWebApi/Controllers/ProjectController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using ExampleWebApi.Domain.DTOs;
+using ExampleWebApi.Domain.Entities;
+using ExampleWebApi.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleWebApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectController : ApiControllerBase
+    {
+        private readonly ExampleDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _environment;
+
+        public ProjectController(ExampleDbContext context, IMapper mapper, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _mapper = mapper;
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            return Ok(_context.Projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProject(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var project = _context.Projects.FirstOrDefault(i => i.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult AddProject([FromBody] ProjectDTO projectDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var project = _mapper.Map<Project>(projectDTO);
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetProject), new { id = project.Id }, project);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProject(int id, [FromBody] ProjectDTO projectDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var project = _context.Projects.FirstOrDefault(i => i.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(projectDTO, project);
+            _context.SaveChanges();
+            return Ok(project);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProject(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var project = _context.Projects.FirstOrDefault(i => i.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            _context.Projects.Remove(project);
+            _context.SaveChanges();
+            return Ok(new { id = id });
+        }
+    }
+}
