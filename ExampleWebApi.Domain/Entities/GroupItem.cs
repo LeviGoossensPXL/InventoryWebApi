3ea3146af7ab3e21c1eb4db5a195a0d3187f11bd
Goossens Levi <Levi.Goossens@student.pxl.be>
2026-01-13 14:54:27 +0100

    allow binding item to a group

---
+++ ExampleWebApi.Domain/Entities/GroupItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebApi.Domain.Entities
+{
+    public class GroupItem
+    {
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+    }
+
+}
