5afcfc5f151654ffaa914508d9f8dc923b6f2ca5
Goossens Levi <Levi.Goossens@student.pxl.be>
2026-01-13 20:20:27 +0100

    Item models

---
+++ ExampleWebApi.Domain/Entities/OwnedItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebApi.Domain.Entities
+{
+    public class OwnedItem
+    {
+        public int Id { get; set; }
+        public Guid OwnerId { get; set; }
+        public User Owner { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ ExampleWebApi.Domain/Entities/WishedItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebApi.Domain.Entities
+{
+    public class WishedItem
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public int Priority { get; set; }
+    }
+}
