6abd6b6b68f3dcbcf2776149fc2fc4fd6b4e1af7
levig <levi.goossens107@gmail.com>
2026-01-18 10:16:00 +0100

    changes 2

--- ExampleWebApi.Infrastructure/Migrations/20260118091247_small_fix.Designer.cs
+++ ExampleWebApi.Infrastructure/Migrations/20260118091247_small_fix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExampleWebApi.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExampleWebApi.Infrastructure.Migrations
 {
     [DbContext(typeof(ExampleDbContext))]
-    partial class ExampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260118091247_small_fix")]
+    partial class small_fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
