8476ecbde42324ac5b883b44ceafbb2f4867238d
levig <levi.goossens107@gmail.com>
2026-01-28 11:54:15 +0100

    small changes

--- SeedSQL.sql
+++ SeedSQL.sql
@@ -7,6 +7,8 @@ VALUES
 (2, 'repair toolkit', 'https://localhost:7027/images/items/c82af8a4-3662-4734-8453-a4e08197efc6.jpg', 'iFixIt', 'pro tech toolkit', 77.41),
 (3, 'monitor arm', 'https://localhost:7027/images/items/45690b8f-3173-4e74-be8f-c773612046bb.jpg', 'Alberenz', 'single monitorarm Donkergrijs', 99.00)
 ON CONFLICT ("Id") DO NOTHING;
+SELECT setval(pg_get_serial_sequence('"Items"', 'Id'),
+              (SELECT MAX("Id") FROM "Items"));
 
 -- =========================
 -- PROJECTS
@@ -16,6 +18,8 @@ VALUES
 (1, 'led cube', 'leds in de vorm van een kubus om mooie effect te tonen door programmatie'),
 (2, 'self balancing cube', 'een kubus die zich zelf kan balanceren op zijn punt')
 ON CONFLICT ("Id") DO NOTHING;
+SELECT setval(pg_get_serial_sequence('"Projects"', 'Id'),
+              (SELECT MAX("Id") FROM "Projects"));
 
 -- =========================
 -- GROUPS
@@ -28,3 +32,5 @@ VALUES
 (4, 'electronics', 'Electronic devices, gadgets, and accessories'),
 (5, 'other', 'Items that do not fit into any specific category')
 ON CONFLICT ("Id") DO NOTHING;
+SELECT setval(pg_get_serial_sequence('"Groups"', 'Id'),
+                                            (SELECT MAX("Id") FROM "Groups"));
